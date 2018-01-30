b75c9b1629423ab7a088c7c37f8a405edd2a68a0
SergeQa <sergii.khrystych@sannacode.com>
2018-01-30 18:55:58 +0200

    src commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,9 @@
+import abstractions.Animal;
+import interfaces.Meowable;
+
+public class Cat extends Animal implements Meowable {
+    private String name;
+    public void eat(){
+
+    }
+}
---
+++ src/main/java/abstractions/Animal.java
@@ -0,0 +1,10 @@
+package abstractions;
+
+public abstract class Animal {
+    public int age;
+    public int weight;
+
+    abstract void run();
+    abstract void eat();
+    abstract void sleep();
+}
